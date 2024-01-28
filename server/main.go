1fffa22f8d948897733a854a4e5ffad1b70c5ed2
Moritz Schramm <mail@mo5.ch>
2024-01-28 10:22:41 +0100

    make docker-compose down fast iss#10; Remove cors and limiter in server (nginx should handle this in the future)

--- server/main.go
+++ server/main.go
@@ -12,11 +12,11 @@ import (
 	"github.com/joho/godotenv"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+	//"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 
-	"github.com/gofiber/fiber/v2/middleware/limiter"
+	//"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/session"
 
 	"github.com/gofiber/storage/redis/v3"
@@ -64,7 +64,7 @@ func main() {
 	app.Use(recover.New(recover.Config{
 		EnableStackTrace: os.Getenv("DEV_ENV") == "true",
 	}))
-	app.Use(limiter.New(limiter.Config{
+	/*app.Use(limiter.New(limiter.Config{
 		// TODO check if KeyGenerator works when behind reverse proxy
 		// TODO checkout sliding window approach
 		SkipSuccessfulRequests: true,
@@ -74,7 +74,7 @@ func main() {
 		AllowHeaders:     "Origin, Content-Type, Accept, X-Csrf",
 		AllowCredentials: true,
 		MaxAge:           3600, // 1 hour caching
-	}))
+	}))*/
 	app.Use(func(c *fiber.Ctx) error { // check if request has custom csrf protection header set
 		// apparently it is enough for a purely ajax req/res scheme to rely on this header
 		// see https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#employing-custom-request-headers-for-ajaxapi
