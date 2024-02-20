c9b1e6683127ec60f415c800af56c03fd4bf3c32
Moritz Schramm <mail@mo5.ch>
2024-02-20 21:33:04 +0100

    minor changes in init script

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-random_string=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 50 | head -c 32)
-
 echo
 echo -e "\033[1012mCreating necessary directories and configuration files...\033[0m"
 echo
 
+database_password=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 50 | head -c 32)
+rm -rf ./database/data # remove already exisiting directory, if present
 mkdir ./database/data
-sed "s/<YOUR PASSWORD HERE>/$random_string/" .example.env > .env
+sed "s/<YOUR PASSWORD HERE>/$database_password/" .example.env > .env
 
 echo
 echo -e "\033[102mBuilding docker images...\033[0m"
@@ -20,7 +20,7 @@ echo
 echo -e "\033[102mMigrating database...\033[0m"
 echo
 
-sleep 20
+sleep 20 # wait for postgres init to complete
 cd ./database
 ./migrate.sh
 cd ..
@@ -30,4 +30,5 @@ docker-compose down
 echo
 echo
 echo -e "\033[92mDONE\033[0m"
-echo -e "\033[102mYou can now start your dockerized dev env with 'docker-compose up'\033[0m"
+echo
+echo -e "\033[102mYou can now start your dockerized dev env using \033[0m docker-compose up"
