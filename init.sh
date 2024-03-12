e5394f08124427d8f751b0a68b73312baac96fe3
Moritz Schramm <mail@mo5.ch>
2024-03-12 17:08:45 +0100

    creating .env before writing to it

--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ echo -e "\033[102mCreating local directories and configuration files...\033[0m"
 
 mkdir -p ./database/internal/postgres ./database/internal/redis ./database/internal/redisinsight ./database/internal/pgadmin # create necessary data directories
 database_password=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 50 | head -c 32) # generate db password
+touch .env
 sed "s/<YOUR PASSWORD HERE>/$database_password/" .example.env > .env # generate env file
 [ ! -f .env ] || export $(grep -v '^#' .env | xargs) # load variables from env file
 
