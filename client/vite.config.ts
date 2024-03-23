92a2555fc13118daf0ff424a45f6ecbb1d1ac565
Moritz Schramm <mail@mo5.ch>
2024-03-23 21:39:51 +0100

    add shadcn component library for UI

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -14,7 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     }
   }
 })
